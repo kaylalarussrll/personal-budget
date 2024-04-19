a0a9d7b8a2b3d93a6756573d8ffaaf92a5beb869
kaylalarussrll <kaylalarussell96@gmail.com>
2024-04-19 13:37:52 -0700

    test to GET from server

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = express();
 const PORT = 3000;
 
 //get to post Hello World when page opens
-
+app.get('/', (req, res, next) => {
+    console.log('Hello, world!');
+    res.send('Hello, World!');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
